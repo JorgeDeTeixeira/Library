85cea4eb41ced534c49ef99fb2889c4d0d5447b0
Jorge Teixeira <jorgedeteixeirademelo@gmail.com>
2024-07-26 19:00:33 -0300

    Ajuste no BookRepository

--- library/src/main/java/br/com/jorge/library/repository/BookRepository.java
+++ library/src/main/java/br/com/jorge/library/repository/BookRepository.java
@@ -1,11 +1,9 @@
 package br.com.jorge.library.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.jorge.library.model.Book;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
-	Optional<Book> findByBookId(Long id);
+	
 }

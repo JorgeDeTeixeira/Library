3fb06590050b4464f13969b471ca32c232d842b5
Jorge Teixeira <jorgedeteixeirademelo@gmail.com>
2024-07-27 11:27:50 -0300

    Melhria no BookRepository

--- library/src/main/java/br/com/jorge/library/repository/BookRepository.java
+++ library/src/main/java/br/com/jorge/library/repository/BookRepository.java
@@ -1,9 +1,25 @@
 package br.com.jorge.library.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.jorge.library.model.Book;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
-	
+	/**
+	 * Encontra um livro pelo seu ISBN.
+	 * 
+	 * @param isbn ISBN do livro.
+	 * @return Livro correspondente ao ISBN, se existir.
+	 */
+	Book findByIsbn(String isbn);
+
+	/**
+	 * Encontra livros por autor.
+	 * 
+	 * @param author Autor dos livros.
+	 * @return Lista de livros correspondentes ao autor.
+	 */
+	List<Book> findByAuthor(String author);
 }

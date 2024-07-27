f66b3c7e96920d032410991d3cab935d65a0c0da
Jorge Teixeira <jorgedeteixeirademelo@gmail.com>
2024-07-27 11:27:33 -0300

    Melhoria no Model

--- library/src/main/java/br/com/jorge/library/model/Book.java
+++ library/src/main/java/br/com/jorge/library/model/Book.java
@@ -8,12 +8,17 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+/**
+ * Representa um livro na biblioteca.
+ */
 @Entity
 @Table(name = "books")
 @Getter
@@ -26,13 +31,27 @@ public class Book {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	/**
+     * O título do livro.
+     */
 	@NotBlank(message = "Title não pode ser vazio.")
 	private String title;
 
-	private String author;
+	/**
+     * O autor do livro. Valor padrão é "Desconhecido".
+     */
+	private String author = "Desconhecido";
 
+	/**
+     * A data de publicação do livro. Não pode ser no futuro.
+     */
+	@NotNull(message = "Published Date não pode ser nulo.")
+	@PastOrPresent(message = "Published Date não pode ser no futuro.")
 	private LocalDate publishedDate;
 
+	/**
+     * O ISBN do livro. Não pode ser vazio.
+     */
 	@NotBlank(message = "ISBN não pode ser vazio.")
 	private String isbn;
 }
